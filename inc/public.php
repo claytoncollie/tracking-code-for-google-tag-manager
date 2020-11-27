54fca9b8a441462e650db32bd05794b48dc30e4f
Clayton Collie <clayton.collie@gmail.com>
2020-11-27 11:15:50 -0600

    Adjust tracking scripts to work with php

--- inc/public.php
+++ inc/public.php
@@ -35,7 +35,11 @@ function tracking_code_for_google_tag_manager_do_the_head_script() {
 		// phpcs:disable
 		'
 		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f); })(window,document,"script","dataLayer","%1$s");</script>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
+		new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
+		\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,\'script\',\'dataLayer\',\'%1$s\');</script>
 		<!-- End Google Tag Manager -->
 		',
 		// phpcs:enable
@@ -68,7 +72,8 @@ function tracking_code_for_google_tag_manager_do_the_body_script() {
 		// phpcs:disable
 		'
 		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=%1$s" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=%1$s"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<!-- End Google Tag Manager (noscript) -->
 		',
 		// phpcs:enable
